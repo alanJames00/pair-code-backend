c6a7b87f05f96449e81ef9faffb1f645d2e78fd9
alanjames00 <alanjames6282@gmail.com>
2023-11-17 10:25:50 +0530

    create a hook tok manage active users to show the newly come user

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ const io = require('socket.io')(4001, {
 io.on('connection', (socket) => {
 
     console.log(`New connection`);
+    
+    // handle room-join
+    socket.on('join-room', message => {
+        console.log(`${socket.id} joined collab ${message.collabId} with username ${message.user}`);
+        socket.join(message.collabId);
+        // broadcast new user joining
+        socket.broadcast.to(message.collabId).emit('user-joined', message.user);
+        
+        // make a fetch request to update in the db
+        fetch('http://localhost:4000/collab/activeHook', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({ activeUser: message.user, collabId: message.collabId})
+        });
+        
+    })
 });
 
 
